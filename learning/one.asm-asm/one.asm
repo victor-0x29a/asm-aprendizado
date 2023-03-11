9a6b028579579b3ec9a228768845c69be8af9cf1
Victor Gomes <cartoonbr495@gmail.com>
2023-03-11 01:45:31 -0400

    My learning

---
+++ learning/one.asm-asm/one.asm
@@ -0,0 +1,18 @@
+section .data 
+    msg db ' oi negao ', 0xA, 0xD ; 
+    len equ $- msg ;
+
+section .text ; 
+
+global _start ; 
+
+_start:
+    mov eax, 0x4 ; 
+    mov ebx, 0x1 ; 
+    mov ecx, msg ; 
+    mov edx, len ; 
+    int 0x80 ;
+
+    mov eax, 0x1 ; 
+    mov ebx, 0x0 ; 
+    int 0x80 ;
